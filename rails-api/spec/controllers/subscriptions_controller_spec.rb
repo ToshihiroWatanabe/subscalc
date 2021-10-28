7582fbb57b95f60d3614fbf7f93f4e5ccc365055
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-10-28 15:43:15 +0900

    update: テストケースを追加

--- rails-api/spec/controllers/subscriptions_controller_spec.rb
+++ rails-api/spec/controllers/subscriptions_controller_spec.rb
@@ -6,12 +6,31 @@ RSpec.describe SubscriptionsController, type: :controller do
     FactoryBot.create(:subscription, :b)
     FactoryBot.create(:subscription, :c)
   end
-  it 'get index' do
-    get :index
-    expect(response.status).to eq(200)
-    puts response.body
-    expect(JSON.parse(response.body)[0]['name']).to eq('テストサブスク')
-    expect(JSON.parse(response.body)[1]['monthEvery']).to eq(12)
-    expect(JSON.parse(response.body)[2]['price']).to eq(90)
+
+  describe 'GET #index' do
+    before { get :index }
+    it 'has a 200 status code' do
+      expect(response.status).to eq(200)
+    end
+    it '0 name' do
+      expect(JSON.parse(response.body)[0]['name']).to eq('テストサブスク')
+    end
+    it '1 monthEvery' do
+      expect(JSON.parse(response.body)[1]['monthEvery']).to eq(12)
+    end
+    it '2 price' do
+      expect(JSON.parse(response.body)[2]['price']).to eq(90)
+    end
+  end
+
+  # describe 'GET #show' do
+  #   before { get :show }
+  # end
+
+  describe 'POST #create' do
+    it 'has a 201 status code' do
+      post :create, params: { name: 'てすと', monthEvery: 6, price: 2480 }
+      expect(response.status).to eq(201)
+    end
   end
 end
