fa55f18d05e3056236fa39ce2c614f08b29c8a0f
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-10-28 13:57:24 +0900

    update: コントローラーの#indexのテストケース作成

--- rails-api/spec/controllers/subscriptions_controller_spec.rb
+++ rails-api/spec/controllers/subscriptions_controller_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 RSpec.describe SubscriptionsController, type: :controller do
   before do
-    @subscription = FactoryBot.create(:subscription)
+    FactoryBot.create(:subscription, :a)
+    FactoryBot.create(:subscription, :b)
+    FactoryBot.create(:subscription, :c)
   end
   it 'get index' do
     get :index
     expect(response.status).to eq(200)
     puts response.body
+    expect(JSON.parse(response.body)[0]['name']).to eq('テストサブスク')
+    expect(JSON.parse(response.body)[1]['monthEvery']).to eq(12)
+    expect(JSON.parse(response.body)[2]['price']).to eq(90)
   end
 end
--- rails-api/spec/factories/subscription.rb
+++ rails-api/spec/factories/subscription.rb
@@ -1,7 +1,21 @@
 FactoryBot.define do
   factory :subscription do
-    name { 'テストサブスク' }
-    monthEvery { 1 }
-    price { 980 }
+    trait :a do
+      name { 'テストサブスク' }
+      monthEvery { 1 }
+      price { 980 }
+    end
+
+    trait :b do
+      name { 'テストサブスク2' }
+      monthEvery { 12 }
+      price { 1980 }
+    end
+
+    trait :c do
+      name { 'テストサブスク3' }
+      monthEvery { 1 }
+      price { 90 }
+    end
   end
 end
